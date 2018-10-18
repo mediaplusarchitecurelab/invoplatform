e69d15b383fc7db62d66fed90ff0716d1af6eccf
mplusalab <43529049+mplusalab@users.noreply.github.com>
2018-10-18 10:20:09 +0800

    Add files via upload

--- inc/about.js
+++ inc/about.js
@@ -4,63 +4,88 @@
                 '<div class="row section-header has-bottom-sep" data-aos="fade-up">'+
                     '<div class="col-full">'+
                         '<h3 class="subhead subhead--dark">雲林科技大學</h3>'+
-                        '<h1 class="display-1 display-1--light">創新創意團隊</h1>'+
+                        '<h1 class="display-1 display-1--light">創新創意推廣模式七階段</h1>'+
                     '</div>'+
                 '</div>'+ // <!-- end section-header -->
 
                 '<div class="row about-desc" data-aos="fade-up">'+
                     '<div class="col-full">'+
                         '<p>'+
-                        '深耕高教11<br>'+
+                        '深耕高教<br>'+
                         '輔導創業<br>'+
                         '</p>'+
                     '</div>'+
                 '</div>'+ //<!-- end about-desc -->
             '<div id="stepbar">'+
                 '<ul class="nav nav-tabs">'+
-                  '<li class="active"><a data-toggle="tab" href="#step1">學習基礎</a></li>'+
-                  '<li><a data-toggle="tab" href="#step2">勇於嘗試</a></li>'+
-                  '<li><a data-toggle="tab" href="#step3">團隊形成</a></li>'+
-                  '<li><a data-toggle="tab" href="#step4">專業諮詢</a></li>'+
-                  '<li><a data-toggle="tab" href="#step5">原型實踐</a></li>'+
-                  '<li><a data-toggle="tab" href="#step6">群眾募資</a></li>'+
-                  '<li><a data-toggle="tab" href="#step7">創業輔導</a></li>'+
+                  '<li class="active"><a data-toggle="tab" href="#step1">STEP1:學習基礎</a></li>'+
+                  '<li><a data-toggle="tab" href="#step2">STEP2:勇於嘗試</a></li>'+
+                  '<li><a data-toggle="tab" href="#step3">STEP3:團隊形成</a></li>'+
+                  '<li><a data-toggle="tab" href="#step4">STEP4:專業諮詢</a></li>'+
+                  '<li><a data-toggle="tab" href="#step5">STEP5:原型實踐</a></li>'+
+                  '<li><a data-toggle="tab" href="#step6">STEP6:群眾募資</a></li>'+
+                  '<li><a data-toggle="tab" href="#step7">STEP7:創業輔導</a></li>'+
                 '</ul>'+
 
                 '<div id="stepcontent" class="tab-content">'+
                   '<div id="step1" class="tab-pane fade in active">'+
-                    '<h3>學習基礎</h3>'+
-                    '<p>教卓中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>教卓中心</h1>'+
+                    '<p>創意發想工作坊<br>'+
+                    '專業領域體驗工作坊<br>'+
+                    '學生自主社團TA、Maker技術學習<p>'+ 
                   '</div>'+
 
                   '<div id="step2" class="tab-pane fade ">'+
-                    '<h3>勇於嘗試</h3>'+
-                    '<p>教卓中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>教卓中心</h1>'+
+                    '<p>師生創客<br>'+
+                    '點子大賽<br>'+
+                    '黑客松<br>'+
+                    '極限工作坊<br>'+
+                    '微型課程</p>'+
                   '</div>'+
 
                   '<div id="step3" class="tab-pane fade">'+
-                    '<h3>團隊形成</h3>'+
-                    '<p>教卓中心、自造者中心、PBL中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>教卓中心、自造者中心、各PBL中心</h1>'+
+                    '<p>創意聯盟<br>'+
+                    '專題聯盟<br>'+
+                    '深化課程<br>'+
+                    '產學執行</p>'+
                   '</div>'+
 
                   '<div id="step4" class="tab-pane fade">'+
-                    '<h3>專業諮詢</h3>'+
-                    '<p>自造者中心、PBL中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>自造者中心、各PBL中心</h1>'+
+                    '<p>青創團隊<br>'+
+                    '校內人力、廠商資源盤整與資訊開放<br>'+
+                    '諮詢機制的建立</p>'+
                   '</div>'+
 
                   '<div id="step5" class="tab-pane fade">'+
-                    '<h3>原型實踐</h3>'+
-                    '<p>自造者中心、PBL中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>自造者中心、各PBL中心</h1>'+
+                    '<p>駐廠創客、創客深耕計畫<br>'+
+                    '創業團隊<br>'+
+                    '原型技術開發<br>'+
+                    '製造流程規劃</p>'+
                   '</div>'+
 
                   '<div id="step6" class="tab-pane fade">'+
-                    '<h3>群眾募資</h3>'+
-                    '<p>育成中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>育成中心</h1>'+
+                    '<p>作品聚焦與商業策略（ＳＭＡＲＴ）<br>'+
+                    '募資影片腳本撰寫製作<br>'+
+                    '使用者經驗分析</p>'+
                   '</div>'+
 
                   '<div id="step7" class="tab-pane fade">'+
-                    '<h3>創業輔導</h3>'+
-                    '<p>育成中心</p>'+
+                    '<h5>主要執行單位</h5>'+
+                    '<h1>育成中心</h1>'+
+                    '<p>法務協助<br>'+
+                    '會計鍵接<br>'+
+                    '行銷推廣</p>'+
                   '</div>'+
                 '</div>'+
             '<div>'+
